3605f0502fe9884f50cd8af371d50102c5cc732a
WahyuNT <wahyuntx28@gmail.com>
2022-04-02 19:53:01 +0700

    0.1
    V 0.1

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderTokovue />
+    <Hero-tokovue />
+    <BannerTokovue />
+    <InstaTokovue />
+    <PartnerTokovue />
+    <FooterTokovue />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import HeaderTokovue from "@/components/HeaderTokovue.vue";
+import HeroTokovue from "@/components/HeroTokovue.vue";
+import BannerTokovue from "@/components/BannerTokovue.vue";
+import InstaTokovue from "@/components/InstaTokovue.vue";
+import PartnerTokovue from "@/components/PartnerTokovue.vue";
+import FooterTokovue from "@/components/FooterTokovue.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HeaderTokovue,
+    HeroTokovue,
+    BannerTokovue,
+    InstaTokovue,
+    PartnerTokovue,
+    FooterTokovue,
+  },
+};
+</script>
